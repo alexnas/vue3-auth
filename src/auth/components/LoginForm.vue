9e98453965ed137ba29556dd3c840c683639c297
Alexnander Nasokin <alexnas2python@gmail.com>
2023-11-24 19:35:52 +0300

    Make view-hide of password field text

--- src/auth/components/LoginForm.vue
+++ src/auth/components/LoginForm.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Icon } from '@iconify/vue'
+
+const passwordShow = ref(false)
+
+const toggleShowPassword = () => {
+  passwordShow.value = !passwordShow.value
+}
+</script>
 
 <template>
   <div class="space-y-4 px-4 py-8 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7">
@@ -17,12 +26,12 @@
         >Email Address</label
       >
     </div>
-    <div class="relative">
+    <div class="relative flex">
       <input
         autocomplete="off"
         id="password"
         name="password"
-        type="password"
+        :type="passwordShow ? 'text' : 'password'"
         class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:border-teal-600 focus:outline-none"
         placeholder="Password"
       />
@@ -31,6 +40,16 @@
         class="peer-placeholder-shown:text-gray-440 absolute -top-3.5 left-0 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
         >Password</label
       >
+      <button
+        class="absolute right-0 top-0 inline-flex h-full w-10 items-center justify-center text-gray-400"
+        @click.prevent="toggleShowPassword"
+      >
+        <Icon
+          class="w-6 min-w-[theme('spacing[5]')] text-3xl text-gray-400 hover:text-teal-600"
+          :icon="passwordShow ? 'mdi:eye-outline' : 'mdi:eye-off-outline'"
+          :inline="true"
+        />
+      </button>
     </div>
     <div class="relative">
       <button
