fddb87bccf386f0659c78a211f71f93f15b0ac4d
Alexnander Nasokin <alexnas2python@gmail.com>
2023-11-26 14:50:52 +0300

    Make Register form markup

--- src/auth/components/RegisterForm.vue
+++ src/auth/components/RegisterForm.vue
@@ -16,6 +16,22 @@ const toggleShowPassword = () => {
     <div class="relative">
       <input
         autocomplete="off"
+        id="name"
+        name="name"
+        type="text"
+        class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:border-teal-600 focus:outline-none"
+        placeholder="Username"
+      />
+      <label
+        for="name"
+        class="peer-placeholder-shown:text-gray-440 absolute -top-3.5 left-0 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
+        >Username</label
+      >
+    </div>
+
+    <div class="relative">
+      <input
+        autocomplete="off"
         id="email"
         name="email"
         type="text"
@@ -28,6 +44,7 @@ const toggleShowPassword = () => {
         >Email Address</label
       >
     </div>
+
     <div class="relative flex">
       <input
         autocomplete="off"
@@ -42,6 +59,23 @@ const toggleShowPassword = () => {
         class="peer-placeholder-shown:text-gray-440 absolute -top-3.5 left-0 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
         >Password</label
       >
+    </div>
+
+    <div class="relative flex">
+      <input
+        autocomplete="off"
+        id="confirm_password"
+        name="confirm_password"
+        :type="passwordShow ? 'text' : 'password'"
+        class="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:border-teal-600 focus:outline-none"
+        placeholder="Confirm Password"
+      />
+      <label
+        for="password"
+        class="peer-placeholder-shown:text-gray-440 absolute -top-3.5 left-0 text-sm text-gray-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-3.5 peer-focus:text-sm peer-focus:text-gray-600"
+        >Confirm Password</label
+      >
+
       <button
         class="absolute right-0 top-0 inline-flex h-full w-10 items-center justify-center text-gray-400"
         @click.prevent="toggleShowPassword"
@@ -63,12 +97,12 @@ const toggleShowPassword = () => {
     </div>
 
     <p class="text-sm font-light text-gray-500">
-      <span>Don't have an account yet? </span>
+      <span>Have an account already? </span>
       <button
         @click="$emit('toggle-auth')"
         class="ml-2 p-2 font-medium text-teal-600 hover:bg-gray-100 hover:text-orange-600"
       >
-        Sign up
+        Login
       </button>
     </p>
   </div>
--- src/auth/views/AuthView.vue
+++ src/auth/views/AuthView.vue
@@ -3,6 +3,7 @@ import { computed, ref } from 'vue'
 import AuthViewContainer from '@/auth/views/AuthViewContainer.vue'
 import AuthFormContainer from '@/auth/views/AuthFormContainer.vue'
 import LoginForm from '@/auth/components/LoginForm.vue'
+import RegisterForm from '../components/RegisterForm.vue'
 
 const pageTitle = 'Authentication'
 const isLoginForm = ref(true)
@@ -10,15 +11,21 @@ const isLoginForm = ref(true)
 const formTitle = computed(() => {
   return isLoginForm.value ? 'Login' : 'Sign up'
 })
+
+const toggleAuth = () => {
+  return (isLoginForm.value = !isLoginForm.value)
+}
 </script>
 
 <template>
   <auth-view-container :pageTitle="pageTitle">
     <auth-form-container :formTitle="formTitle">
       <div v-if="isLoginForm">
-        <login-form />
+        <login-form @toggleAuth="toggleAuth" />
       </div>
-      <div v-else>Register Form</div>
+      <div v-else>
+        <register-form @toggleAuth="toggleAuth" />
+      </div>
     </auth-form-container>
   </auth-view-container>
 </template>
